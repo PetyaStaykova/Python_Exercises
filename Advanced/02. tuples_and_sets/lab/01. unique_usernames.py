0e0c13420346b6e4bd32e1438280bcf6735b5777
PetyaStaykova <pk.staykova@gmail.com>
2021-10-12 18:43:38 -0400

    Added Tuple and Set problems

--- Advanced/02. tuples_and_sets/lab/01. unique_usernames.py
+++
@@ -1,8 +0,0 @@
-n_lines = int(input())
-
-unique_usernames = set()
-
-for _ in range(n_lines):
-    username = input()
-    unique_usernames.add(username)
-[print(username) for username in unique_usernames]
